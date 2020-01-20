d3ca4a675d9e3398825e91b4a60bc4fe16910390
Haoran Zhang <zhangh87@mcmaster.ca>
2020-01-20 18:00:55 -0500

    Fixes bug in debiasing code

--- bash_scripts/train_adv_clinical_BERT.sh
+++ bash_scripts/train_adv_clinical_BERT.sh
@@ -1,41 +1,45 @@
 #!/bin/bash
 #SBATCH --partition gpu
-#SBATCH --gres gpu:4
+#SBATCH --gres gpu:8
 #SBATCH -c 8
-#SBATCH --output train_adv.log
-#SBATCH --mem 200gb
+#SBATCH --output train_adv%A.log
+#SBATCH --mem 160gb
 set -e
 source activate hurtfulwords
 
 BASE_DIR="/h/haoran/projects/HurtfulWords"
-OUTPUT_DIR="/h/haoran/projects/HurtfulWords/data/"
-SCIBERT_DIR="/scratch/gobi1/haoran/shared_data/BERT_DeBias/models/SciBERT"
+OUTPUT_DIR="/scratch/hdd001/home/haoran/shared_data/BERT_DeBias/data/"
+SCIBERT_DIR="/scratch/hdd001/home/haoran/shared_data/BERT_DeBias/models/SciBERT"
 mkdir -p "$OUTPUT_DIR/models/"
+DOMAIN="$1"
 
 cd "$BASE_DIR/scripts" 
 
 python adversarial_finetune_on_pregen.py \
 	--pregenerated_data "$OUTPUT_DIR/pregen_epochs/128/" \
-	--output_dir "$OUTPUT_DIR/models/adv_clinical_BERT_1_epoch_128/" \
+	--output_dir "$OUTPUT_DIR/models/adv_clinical_BERT_${DOMAIN}_1_epoch_128/" \
 	--bert_model "$SCIBERT_DIR" \
+	--do_lower_case \
+	--epochs 1 \
+	--train_batch_size 64\
+	--seed 123 \
+	--domain_of_interest "$DOMAIN" \
+	--lambda_ 1.0 \
+	--num_layers 3\
+    --use_new_mapping
+
+python adversarial_finetune_on_pregen.py \
+	--pregenerated_data "$OUTPUT_DIR/pregen_epochs/512/" \
+	--output_dir "$OUTPUT_DIR/models/adv_clinical_BERT_${DOMAIN}_1_epoch_512/" \
+	--bert_model "$OUTPUT_DIR/models/adv_clinical_BERT_${DOMAIN}_1_epoch_128/" \
 	--do_lower_case \
 	--epochs 1 \
 	--train_batch_size 32\
 	--seed 123 \
-	--domain_of_interest "gender" \
+	--domain_of_interest "$DOMAIN" \
 	--lambda_ 1.0 \
 	--num_layers 3\
+    --use_new_mapping
 
-python adversarial_finetune_on_pregen.py \
-	--pregenerated_data "$OUTPUT_DIR/pregen_epochs/512/" \
-	--output_dir "$OUTPUT_DIR/models/adv_clinical_BERT_1_epoch_512/" \
-	--bert_model "$OUTPUT_DIR/models/adv_clinical_BERT_1_epoch_128/" \
-	--do_lower_case \
-	--epochs 1 \
-	--train_batch_size 16\
-	--seed 123 \
-	--domain_of_interest "gender" \
-	--lambda_ 1.0 \
-	--num_layers 3\
 
-rm -rf "$OUTPUT_DIR/models/adv_clinical_BERT_1_epoch_128/"
+rm -rf "$OUTPUT_DIR/models/adv_clinical_BERT_${DOMAIN}_1_epoch_128/"
